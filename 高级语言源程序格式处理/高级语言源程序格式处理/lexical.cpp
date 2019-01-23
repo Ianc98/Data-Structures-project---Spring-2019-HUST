754c562d690cc00962d6ebd1caf3d6ce3be7c872
HomePC <zy1450510494@icloud.com>
2019-01-23 22:13:58 +0800

    Unfinished

--- 高级语言源程序格式处理/高级语言源程序格式处理/lexical.cpp
+++ 高级语言源程序格式处理/高级语言源程序格式处理/lexical.cpp
@@ -5,5 +5,11 @@ char * token_text;
 int gettoken(FILE * fp)
 {
 	token_text = NULL;
+	while ((c = fgetc(fp)) == ' ');	//过滤掉空白符号，如果考虑报错位置，对回车需要单独
+									//处理，每次回车，设置一个行数计数器加1
+	if ((c >= 'A'&&c <= "Z") || (c >= 'a'&&c <= 'z'))
+	{
+
+	}
 	return 0;
 }
