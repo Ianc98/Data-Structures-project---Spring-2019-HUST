e37b236c0a77944c7e7d76f3615b26c8d91186e6
易 周 <zy1450510494@icloud.com>
2019-01-25 22:30:19 +0800

    Unfinished

--- 高级语言源程序格式处理/高级语言源程序格式处理/lexical.cpp
+++ 高级语言源程序格式处理/高级语言源程序格式处理/lexical.cpp
@@ -1,34 +1,36 @@
 #include "lexical.h"
 
 char token_text[MAXLEN];
-keyword HashKey[23][7] = 
+keyword HashKey[25][11] = 
 {
-	{{}},
-	{},
+	{{"_Alignas",_ALIGNAS},{"_Alignof",_ALIGNOF},{"_Atomic",_ATOMIC},{"_Bool",_BOOL},
+	{"_Complex",_COMPLEX},{"_Generic",_GENERIC},{"_Imaginary",_IMAGINARY},{"_Noreturn",_NORETURN},
+	{"_Static_assert",_STATIC_ASSERT},{"_Thread_local",_THREAD_LOCAL},{'\0',IDENT}},
+	{{'\0',IDENT}},
 	{{"auto",AUTO},{'\0',IDENT}},
-{{"break",BREAK},{'\0',IDENT}},
-{{"case",CASE},{"char",CHAR},{"const",CONST},{"continue",CONTINUE},{'\0',IDENT}},
-{{"default",DEFAULT},{"do",DO},{"double",DOUBLE},{'\0',IDENT}},
-{{"else",ELSE},{"enum",ENUM},{"extern",EXTERN},{'\0',IDENT}},
-{{"float",FLOAT},{"for",FOR},{'\0',IDENT}},
-{{"goto",GOTO},{'\0',IDENT}},
-{{'\0',IDENT}},
-{{"if",IF},{"inline",INLINE},{"int",INT},{'\0',IDENT}},
-{{'\0',IDENT}},
-{{'\0',IDENT}},
-{{"long",LONG},{'\0',IDENT}},
-{{'\0',IDENT}},
-{{'\0',IDENT}},
-{{'\0',IDENT}},
-{{'\0',IDENT}},
-{{'\0',IDENT}},
-{{"register",REGISTER},{"restrict",RESTRICT},{"return",RETURN},{'\0',IDENT}},
-{{"short",SHORT},{"signed",SIGNED},{"sizeof",SIZEOF},{"static",STATIC},
-{"struct",STRUCT},{"switch",SWITCH},{'\0',IDENT}},
-{{"typedef",TYPEDEF},{'\0',IDENT}},
-{{"union",UNION},{"unsigned",UNSIGNED},{'\0',IDENT}},
-{{"void",VOID},{"volatile",VOLATILE},{'\0',IDENT}},
-{{"while",WHILE},{'\0',IDENT}}
+	{{"break",BREAK},{'\0',IDENT}},
+	{{"case",CASE},{"char",CHAR},{"const",CONST},{"continue",CONTINUE},{'\0',IDENT}},
+	{{"default",DEFAULT},{"do",DO},{"double",DOUBLE},{'\0',IDENT}},
+	{{"else",ELSE},{"enum",ENUM},{"extern",EXTERN},{'\0',IDENT}},
+	{{"float",FLOAT},{"for",FOR},{'\0',IDENT}},
+	{{"goto",GOTO},{'\0',IDENT}},
+	{{'\0',IDENT}},
+	{{"if",IF},{"inline",INLINE},{"int",INT},{'\0',IDENT}},
+	{{'\0',IDENT}},
+	{{'\0',IDENT}},
+	{{"long",LONG},{'\0',IDENT}},
+	{{'\0',IDENT}},
+	{{'\0',IDENT}},
+	{{'\0',IDENT}},
+	{{'\0',IDENT}},
+	{{'\0',IDENT}},
+	{{"register",REGISTER},{"restrict",RESTRICT},{"return",RETURN},{'\0',IDENT}},
+	{{"short",SHORT},{"signed",SIGNED},{"sizeof",SIZEOF},{"static",STATIC},
+	{"struct",STRUCT},{"switch",SWITCH},{'\0',IDENT}},
+	{{"typedef",TYPEDEF},{'\0',IDENT}},
+	{{"union",UNION},{"unsigned",UNSIGNED},{'\0',IDENT}},
+	{{"void",VOID},{"volatile",VOLATILE},{'\0',IDENT}},
+	{{"while",WHILE},{'\0',IDENT}}
 };
 
 int gettoken(FILE * fp)
@@ -38,19 +40,19 @@ int gettoken(FILE * fp)
 	int j = 0;
 	while ((c = fgetc(fp)) == ' ');	//过滤掉空白符号，如果考虑报错位置，对回车需要单独
 									//处理，每次回车，设置一个行数计数器加1
-	if ((c >= 'A'&&c <= 'Z') || (c >= 'a'&&c <= 'z'))
+	if ((c >= 'A'&&c <= 'Z') || (c >= 'a'&&c <= 'z') || c == '_')
 	{
 		do
 		{
 			token_text[i] = c;		//将c的值依次放入标识符串	
 			i++;
 		} while (((c = fgetc(fp)) >= 'A'&&c <= 'Z') || (c >= 'a'&&c <= 'z') ||
-			(c >= '0'&&c <= '9'));
+			(c >= '0'&&c <= '9') || c == '_');
 		//如果c是字母或数字
 		ungetc(c, fp);					//退回多读的字符到文件输入缓冲区
 		c = token_text[0];
-		if (c >= 'A'&&c <= 'Z' || c > 'w') return IDENT;
-		i = c - 'a';
+		if (c != '_' && (c<'a' || c>'w')) return IDENT;
+		i = c - '_';
 		while (strcmp(token_text, HashKey[i][j].key) && HashKey[i][j].key != 0)
 			j++;
 		return HashKey[i][j].c;
@@ -67,6 +69,12 @@ int gettoken(FILE * fp)
 	}
 	switch (c)
 	{
+	case '=':
+		c = fgetc(fp);
+		if (c == '=') return EQ;
+		ungetc(c, fp);
+		return ASSIGN;
+		case
 	default:
 		break;
 	}
