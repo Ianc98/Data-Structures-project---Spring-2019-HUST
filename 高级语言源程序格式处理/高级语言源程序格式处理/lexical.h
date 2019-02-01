c3f91e29a9b556401988981749af834d8f576b8e
易 周 <zy1450510494@icloud.com>
2019-02-01 16:45:16 +0800

    Unfinished

--- 高级语言源程序格式处理/高级语言源程序格式处理/lexical.h
+++ 高级语言源程序格式处理/高级语言源程序格式处理/lexical.h
@@ -6,6 +6,7 @@
 
 #ifndef _LEXICAL_H
 #define _LEXICAL_H
+//防止重复包含该头文件
 
 #include<stdio.h>
 #include<malloc.h>
@@ -18,18 +19,27 @@ struct keyword
 	char key[15];
 	enum token_kind c;
 };
+//Hash表的数据元素
 enum token_kind
 {
 	ERROR_TOKEN, AUTO, BREAK, CASE, CHAR, CONST, CONTINUE, DEFAULT, DO, DOUBLE, ELSE, ENUM, EXTERN,
 	FLOAT, FOR, GOTO, IF, INLINE, INT, LONG, REGISTER, RESTRICT, RETURN, SHORT, SIGNED, SIZEOF,
 	STATIC, STRUCT, SWITCH, TYPEDEF, UNION, UNSIGNED, VOID, VOLATILE, WHILE, _ALIGNAS, _ALIGNOF,
 	_ATOMIC, _BOOL, _COMPLEX, _GENERIC, _IMAGINARY, _NORETURN, _STATIC_ASSERT, _THREAD_LOCAL, IDENT,
-	INT_CONST, EQ, ASSIGN, PLUS, PLUSPLUS, SEMI
+	INT_CONST, FLOAT_CONST, EQ, ASSIGN, PLUS, PLUSPLUS, SEMI
 };
+//定义枚举类型，使不同类型的单词得到唯一的标识码
 
-extern char token_text[MAXLEN];
-extern keyword HashKey[25][11];
+extern int num;						//行数计数器，记录当前已分析到源代码的第几行
+extern char token_text[MAXLEN];		//存放单词值的字符串
+extern keyword HashKey[25][11];		//存放关键字及其标识码的Hash表
 
+/**********************************************
+*函数名称：gettoken
+*函数功能：对源代码进行词法分析
+*传入参数：源代码文件指针fp
+*返回值：单词标识码
+**********************************************/
 int gettoken(FILE * fp);
 
 #endif // !_LEXICAL_H
--- 高级语言源程序格式处理/高级语言源程序格式处理/main.cpp
+++ 高级语言源程序格式处理/高级语言源程序格式处理/main.cpp
@@ -4,18 +4,56 @@ int main(void)
 	char filename[30]; FILE * fp;
 	int status;
 	printf("输入文件名：");
-	scanf("%s", &filename);
-	if (!(fp = fopen(filename, "r")))
+	scanf_s("%s", filename, 30);
+	if (fopen_s(&fp,filename,"r"))
 	{
 		printf("文件打开失败！\n");
 		getchar(); return 0;
 	}
+	printf("\n");
 	printf("	单词类别		单词值\n");
 	while (!feof(fp))
 	{
 		status = gettoken(fp);
 		if (status >= AUTO && status <= _THREAD_LOCAL)
-			printf("	关键字");
+			printf("	关键字			%s\n", token_text);
+		switch (status)
+		{
+		case IDENT:
+			printf("	标识符			%s\n", token_text);
+			break;
+		case INT_CONST:
+			printf("	整型常量		%s\n", token_text);
+			break;
+		case FLOAT_CONST:
+			printf("	浮点型常量		%s\n", token_text);
+			break;
+		case EQ:
+			printf("	等号			==\n");
+			break;
+		case ASSIGN:
+			printf("	赋值号			=\n");
+			break;
+		case PLUS:
+			printf("	加号			+\n");
+			break;
+		case PLUSPLUS:
+			printf("	自增号			++\n");
+			break;
+		case SEMI:
+			printf("	分号			;\n");
+			break;
+		case ERROR_TOKEN:
+			printf("\n");
+			printf("出错！出错行数：%d\n", num);
+			printf("\n");
+			printf("按任意键继续...");
+			getchar(); getchar();
+			return 0;
+		}
 	}
+	printf("\n");
+	printf("按任意键继续...");
+	getchar(); getchar();
 	return 1;
 }
