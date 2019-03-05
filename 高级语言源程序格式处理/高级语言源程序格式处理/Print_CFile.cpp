ec234bb8c5c0dfcee8f552fd94f6e0d4e88f16f5
易 周 <zy1450510494@icloud.com>
2019-03-05 11:55:13 +0800

    缩进不收行首先前打印的制表符影响。 自动过滤代码中的多余空格，仅保留一个空格。 过滤多余的空白行。 过滤多余的制表符。

--- 高级语言源程序格式处理/高级语言源程序格式处理/Print_CFile.cpp
+++ 高级语言源程序格式处理/高级语言源程序格式处理/Print_CFile.cpp
@@ -11,13 +11,34 @@ Status Print_CFile(FILE * fp)
 		indent0 = printList.front().indent;
 		printList.pop();
 		while (!printList.empty() && line == printList.front().num) printList.pop();
+		//将起始行号相同的所有元素全部出队列，以免冲突
 		while (!printList.empty() && line < printList.front().num)
 		{
-			for (i = 0; i < indent0; i++)
-				fputc('\t', fp1);
-			while ((c = fgetc(fp)) != '\n')
+			if ((c = fgetc(fp)) == '#')
+			{
+				do
+				{
+					fputc(c, fp1);
+				} while ((c = fgetc(fp)) != '\n');
 				fputc(c, fp1);
-			fputc(c, fp1);
+			}
+			else if (c == '\n');
+			else
+			{
+				for (i = 0; i < indent0; i++)
+					fputc('\t', fp1);
+				if (c == '\t')
+					while ((c = fgetc(fp)) == '\t');
+				ungetc(c, fp);
+				while ((c = fgetc(fp)) != '\n')
+				{
+					while (c == '\t') c = fgetc(fp);
+					if (c == ' ') fputc(c, fp1);
+					while (c == ' ') c = fgetc(fp);
+					fputc(c, fp1);
+				}
+				fputc(c, fp1);
+			}
 			line++;
 		}
 	}
@@ -25,6 +46,7 @@ Status Print_CFile(FILE * fp)
 		fputc('\t', fp1);
 	while ((c = fgetc(fp)) != EOF)
 		fputc(c, fp1);
+	//打印缩进为队列中最后一个元素存放值的行
 	fclose(fp); fclose(fp1);
 	return OK;
 }
